53a35a733d27b952f2bbeb20f525196f5fffb630
Susan Wiens <susanmichellewiens@gmail.com>
2021-03-01 15:08:04 -0600

    intial commit

---
+++ chinook.session.sql
@@ -0,0 +1,8 @@
+-- non_usa_customers.sql: Provide a query showing Customers (just their full names, customer ID and country) who are not in the US.
+
+-- SELECT CustomerId, FirstName, LastName, Country 
+-- FROM Customer 
+-- WHERE Country NOT IN ("USA")
+
+brazil_customers.sql: Provide a query only showing the Customers from Brazil.
+
