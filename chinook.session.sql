f33c226dfa8623e76415c082be17a53f76cbb7d1
Susan Wiens <susanmichellewiens@gmail.com>
2021-03-04 09:36:40 -0600

    sql queries

--- chinook.session.sql
+++ chinook.session.sql
@@ -1,11 +1,13 @@
 -- non_usa_customers.sql: Provide a query showing Customers (just their full names, customer ID and country) who are not in the US.
-
 -- SELECT CustomerId, FirstName, LastName, Country 
 -- FROM Customer 
 -- WHERE Country NOT IN ("USA")
 
 -- brazil_customers.sql: Provide a query only showing the Customers from Brazil.
-SELECT CustomerId, FirstName, LastName, Country
-FROM Customer
-WHERE Country IN ("Brazil")
+-- SELECT CustomerId, FirstName, LastName, Country
+-- FROM Customer
+-- WHERE Country IN ("Brazil")
 
+-- brazil_customers_invoices.sql: Provide a query showing the Invoices of customers who are from Brazil. The resultant table should show the customer's full name, Invoice ID, Date of the invoice and billing country.
+SELECT FirstName, LastName, Invoice,
+FROM 
