560ef412f27fa35bce7c5dc4066c76186958d6e0
Susan Wiens <susanmichellewiens@gmail.com>
2021-03-09 12:36:33 -0600

    sql #5

--- chinook.session.sql
+++ chinook.session.sql
@@ -16,8 +16,11 @@
 -- WHERE BillingCountry IN ("Brazil")
 
 -- sales_agents.sql: Provide a query showing only the Employees who are Sales Agents.
-SELECT FirstName
-FROM Employee
-WHERE Title LIKE "%Sales%"
+-- SELECT FirstName
+-- FROM Employee
+-- WHERE Title LIKE "%Sales%"
 
+unique_invoice_countries.sql: Provide a query showing a unique/distinct list of billing countries from the Invoice table.
+SELECT DISTINCT BillingCountry
+FROM Invoice
 
